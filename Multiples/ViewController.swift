b86876d8ac79e6d48e12c6893616c1bb26f1fab8
Hector Rios <oj.rios@gmail.com>
2016-02-10 12:44:41 +0100

    Completed Devslopes Multiples Exercise

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var multiplesTextField: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var messageLbl: UILabel!
+    @IBOutlet weak var addBtn: UIButton!
+    
+    //MARK: Properties
+    var runningSum: Int = 0
+    var multipleVal: Int = 0
+    let START_MESSAGE = "Press Add to add!"
+    let MAX_CALCULATIONS = 10
+    var numberOfCalculations = 0
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,57 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: IBActions
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton) {
+        if multiplesTextField.text != nil && !multiplesTextField.text!.isEmpty {
+            
+            if  (Int(multiplesTextField.text!) != nil) {
+                
+                self.multipleVal = Int(multiplesTextField.text!)!
+                
+                //Prepare to play
+                self.logoImage.hidden = true
+                self.playBtn.hidden = true
+                self.multiplesTextField.hidden = true
+                
+                self.addBtn.hidden = false
+                self.messageLbl.hidden = false
+            }
+            
+        }
+    }
+    
 
+    @IBAction func onAddBtnPressed(sender: UIButton) {
+        
+        if (numberOfCalculations < self.MAX_CALCULATIONS) {
+            let currentRunningSum = self.runningSum
+            self.runningSum += self.multipleVal
+            
+            let equation: String = "\(currentRunningSum) + \(multipleVal) = \(runningSum)"
+            
+            self.messageLbl.text = equation
+            self.numberOfCalculations++
+        } else {
+            //Reset the Game
+            //Prepare to play
+            self.logoImage.hidden = false
+            self.playBtn.hidden = false
+            self.multiplesTextField.text = ""
+            self.multiplesTextField.hidden = false
+            
+            self.addBtn.hidden = true
+            self.messageLbl.text = self.START_MESSAGE
+            self.messageLbl.hidden = true
+            
+            self.multipleVal = 0
+            self.runningSum = 0
+            self.numberOfCalculations = 0
+        }
+        
+    }
 
 }
 
